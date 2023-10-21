9b07154a8a4080d5af7e38489aa3df24182dc700
sandeshad100 <sandeshadhikari20076@gmail.com>
2023-10-21 16:49:05 +0545

    getTask, add task

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import userRouter from "./routes/user.js";
+import taskRouter from "./routes/task.js";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 //using routes
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/task", taskRouter);
 
 app.get("/", (req, res) => {
   res.send("Nice Working");
---
+++ routes/task.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { getMyTask, newTask } from "../controllers/task.js";
+import { isAuthenticated } from "../middlewares/auth.js";
+
+const router = express.Router();
+
+router.post("/new", isAuthenticated, newTask);
+router.get("/my", isAuthenticated, getMyTask);
+export default router;
