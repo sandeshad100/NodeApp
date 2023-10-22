74f0a1cda5e6336c0be2bf52b8ba6a6f0adc1afe
sandeshad100 <sandeshadhikari20076@gmail.com>
2023-10-22 09:24:26 +0545

    Try catch for async function

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,10 @@
 import express from "express";
 
-import {
-  getAllUsers,
-  register,
-  login,
-  getMyProfile,
-  logout,
-} from "../controllers/user.js";
+import { register, login, getMyProfile, logout } from "../controllers/user.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.get("/all", getAllUsers);
 router.post("/new", register);
 router.post("/login", login);
 router.get("/logout", logout);
