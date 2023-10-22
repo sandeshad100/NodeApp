13b4e134e220703e68c7f24fdd8afcafe0ccbb9a
sandeshad100 <sandeshadhikari20076@gmail.com>
2023-10-22 13:40:00 +0545

    push

--- data/database.js
+++ data/database.js
@@ -6,7 +6,7 @@ export const connectDB = () => {
     .connect(process.env.MONGO_URI, {
       dbName: "backendapi",
     })
-    .then(() => console.log("Database Connected"))
+    .then((c) => console.log(`Database Connected with ${c.connection.host}`))
     .catch((e) => console.log(e));
 };
 // DB CONNECTION END
